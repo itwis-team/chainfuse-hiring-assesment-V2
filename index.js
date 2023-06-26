7234c5355021376d2de4c7348c633644c26c7744
Ivan Vlasov <vlasovim@itwis.org>
2023-06-26 18:48:17 +0700

    mongo connect fix

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const cors = require("cors");
 const express = require("express");
 const router = require("./router");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => {
+  const app = express();
 
-app.use(express.json());
-app.use(cors());
-
-app.use("/api", router);
-
-app.listen(8000, () => {
-  console.log("Server started at port 8000");
+  app.use(express.json());
+  app.use(cors());
+  
+  app.use("/api", router);
+  
+  app.listen(8000, () => {
+    console.log("Server started at port 8000");
+  });
 });
